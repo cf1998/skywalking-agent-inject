985e393bd5e2a039a09240cad765bccd601c3a8d
Your Name <you@example.com>
2022-02-17 17:00:27 +0800

    123

---
+++ README.md
@@ -0,0 +1,1 @@
+"# skywalking-agent-inject" 
---
+++ pkg/webhook.go
@@ -0,0 +1,87 @@
+package pkg
+
+import (
+	"io"
+	admissionv1 "k8s.io/api/admission/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/klog"
+	"net/http"
+)
+
+var (
+	runtimeScheme = runtime.NewScheme()
+	codeFactory   = serializer.NewCodecFactory(runtimeScheme)
+	deserializer  = codeFactory.UniversalDeserializer()
+)
+
+
+type WhSvrParam struct {
+	Port     int
+	CertFile string
+	KeyFile  string
+}
+
+type WebhookServer struct {
+	Server              *http.Server // http server
+	WhiteListRegistries []string     // 白名单的镜像仓库列表
+}
+
+func (s *WebhookServer) Handler(w http.ResponseWriter, r *http.Request) {
+	var body []byte
+	// 判断接收到请求体是否为为空
+	if r.Body != nil {
+		if data, err := io.ReadAll(r.Body); err == nil {
+			body = data
+		}
+	}
+
+	// 判断body长度是否0
+	if len(body) == 0 {
+		http.Error(w, "no body found", http.StatusBadRequest)
+		return
+	}
+	// 校验内容类型是否正确
+	contentType := r.Header.Get("Content-Type")
+	if contentType != "application/json" {
+		http.Error(w, "invalid Content-Type, want `application/json`", http.StatusUnsupportedMediaType)
+		return
+	}
+	var admissionResponse *admissionv1.AdmissionResponse
+	requestedAdmissionReview := admissionv1.AdmissionReview{}
+
+	if _, _, err := deserializer.Decode(body, nil, &requestedAdmissionReview); err != nil {
+		klog.Errorf("Can't decode body: %v", err)
+		admissionResponse = &admissionv1.AdmissionResponse{
+			Result: &metav1.Status{
+				Code:    http.StatusInternalServerError,
+				Message: err.Error(),
+			},
+		}
+	} else {
+		// 序列化成功，也就是说获取到了请求的 AdmissionReview 的数据
+		if r.URL.Path == "/mutate" {
+			klog.Infof("已拿到数据 %s",&requestedAdmissionReview)
+			admissionResponse = s.mutate(&requestedAdmissionReview)
+		}
+	}
+	// 构造返回的 AdmissionReview 这个结构体
+	responseAdmissionReview := admissionv1.AdmissionReview{}
+	// admission/v1
+	responseAdmissionReview.APIVersion = requestedAdmissionReview.APIVersion
+	responseAdmissionReview.Kind = requestedAdmissionReview.Kind
+	if admissionResponse != nil {
+		responseAdmissionReview.Response = admissionResponse
+		if requestedAdmissionReview.Request != nil { // 返回相同的 UID
+			responseAdmissionReview.Response.UID = requestedAdmissionReview.Request.UID
+		}
+}}
+
+
+func (s *WebhookServer) mutate(ar *admissionv1.AdmissionReview) *admissionv1.AdmissionResponse {
+	req := ar.Request
+	klog.Infof("AdmissionReview for Kind=%s, Namespace=%s Name=%s UID=%s",
+		req.Kind.Kind, req.Namespace, req.Name, req.UID)
+	return nil
+}
